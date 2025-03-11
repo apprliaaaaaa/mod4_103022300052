313ca041e2013418a8655d3f6a7ac07e813ec4e3
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 06:50:57 +0700

    Menambahkan class KodeProduk

--- KodeProduk.cs
+++ KodeProduk.cs
@@ -8,5 +8,30 @@ namespace mod4_103022300052
 {
     class KodeProduk
     {
+        private static readonly Dictionary<string, string> kodeProDict = new Dictionary<string, string>()
+        {
+            {"Laptop", "E100" },
+            {"Smartphone", "E200" },
+            {"Tablet", "E300" },
+            {"Headset", "E400" },
+            {"Keyboard", "E500" },
+            {"Mouse", "E600" },
+            {"Printer", "E700" },
+            {"Monitor", "E800" },
+            {"Smarthwatch", "E900" },
+            {"Kamera", "E1000" }
+        };
+
+        public static string GetKodeProduk(string namaProduk)
+        {
+            if (kodeProDict.ContainsKey(namaProduk))
+            {
+                return kodeProDict[namaProduk];
+            }
+            else
+            {
+                return "Produk tidak ditemukan";
+            }
+        }
     }
 }

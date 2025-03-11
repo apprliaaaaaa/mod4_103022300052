34c262f633b834b2463fd378466afab8a5ca3b13
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 06:44:37 +0700

    inisialisasi projek awal

---
+++ KodeProduk.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod4_103022300052
+{
+    class KodeProduk
+    {
+    }
+}
